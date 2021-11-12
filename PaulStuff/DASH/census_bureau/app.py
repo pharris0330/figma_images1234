05e34f40a230238ed772efaa04ac771cfc95d072
paul harris <pharris0330@gmail.com>
2021-11-12 11:21:16 -0500

    commit #1

--- PaulStuff/DASH/census_bureau/app.py
+++ PaulStuff/DASH/census_bureau/app.py
@@ -5,4 +5,5 @@ app = dash.Dash(__name__, suppress_callback_exceptions=True,
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
+
 server = app.server
